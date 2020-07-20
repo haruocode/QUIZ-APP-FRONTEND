9811965888cd4b6db4ee7c8db9a75e6352eaa9ef
haruocode <takaron1979@yahoo.co.jp>
2020-07-20 11:25:44 +0900

    8.クイズ画面のレイアウトデザイン | 4択クイズWebアプリを作る

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,15 +14,18 @@ const routes: Array<RouteConfig> = [
   {
     path: '/list',
     name: 'List',
-    component: List
-  },
-  {
-    path: '/about',
-    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "list" */ '../views/List.vue')
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "quiz" */ '../views/Quiz.vue')
   }
 ]
 
---
+++ src/views/Quiz.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <h1 class="text-center">クイズタイトル</h1>
+    <div class="question-container">
+      <div class="question-number">Q1</div>
+      <div class="question-box">日本で一番高い山の名前は？</div>
+
+      <!-- 4択 -->
+      <div id="answers">
+        <v-row>
+          <v-col cols="6">
+            <v-btn x-large block dark color="blue">A. 富士山</v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn x-large block dark outlined color="blue">B. 六甲山</v-btn>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="6">
+            <v-btn x-large block dark outlined color="blue">C. 阿蘇山</v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn x-large block dark outlined color="blue">D. 函館山</v-btn>
+          </v-col>
+        </v-row>
+      </div>
+
+      <div class="answer-btn">
+        <v-btn depressed x-large block color="primary">回答する</v-btn>
+      </div>
+
+    </div>
+
+  </v-container>
+</template>
+
+<style>
+  #answers {
+    margin-top: 10px;
+  }
+
+  .answer-btn {
+    margin-top: 14px;
+  }
+
+  .question-container {
+    margin: 0px 30px;
+  }
+
+  .question-number {
+    padding-left: 6px;
+    font-size: 30px;
+  }
+
+  .question-box {
+    padding: 10px;
+    border: 2px solid #000;
+    height: 220px;
+    font-size: 30px;
+  }
+</style>
